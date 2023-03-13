9d9bd3d1e169bbb2a907977e08fd88413f8ceb16
vaseles <vaselesstolarov@gmail.com>
2023-03-13 23:53:10 +0600

    change product page structure

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -7,7 +7,6 @@ import ProductItem from '../../components/ui/product-item/ProductItem';
 import Layout from './../../components/Layout/Layout';
 
 const Shop:FC = () => { 
-   const h2ref = useRef(null);
    const [count, setCount] = useState(12)
 
    const {data: products, isLoading } = useQuery(['products', count], () => ProductService.getProducts(count), {
@@ -15,23 +14,7 @@ const Shop:FC = () => {
    }
    )
 
-   useLayoutEffect(() => {
-      // h2ref.current.scrollIntoView();
-    }, []);
-   // const [products, setProducts] = useState<any[]>([])
-   // const [data, setData] = useState<any[]>([])
-   // const [error, setError] = useState<string>('')
-   // const [isLoading, setIsLoading] = useState<boolean>(false)
-
-   // useEffect(() => {
-   //    ProductService.getProducts()
-   //       .then((res:any) => {
-   //          setProducts(res.data.products); 
-   //          setData(res.data);
-   //       })
-   //       .catch((err:any) => console.error(err))
-   //       .finally(() => setIsLoading(true))
-   // }, [])
+   
 
   return (
    <Layout title='Shop The Collection'>
---
+++ src/pages/Shop/info.ts
@@ -0,0 +1,14 @@
+// const [products, setProducts] = useState<any[]>([])
+   // const [data, setData] = useState<any[]>([])
+   // const [error, setError] = useState<string>('')
+   // const [isLoading, setIsLoading] = useState<boolean>(false)
+
+   // useEffect(() => {
+   //    ProductService.getProducts()
+   //       .then((res:any) => {
+   //          setProducts(res.data.products); 
+   //          setData(res.data);
+   //       })
+   //       .catch((err:any) => console.error(err))
+   //       .finally(() => setIsLoading(true))
+   // }, [])
